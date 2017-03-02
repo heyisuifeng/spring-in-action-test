47970909ce7c936d3054c209292be5d92e8b322c
heyisuifeng <1018739994@qq.com>
2017-03-02 22:03:28 +0800

    clear xml

---
+++ src/main/java/com/ace/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ace;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * Created by yecanyi on 2017/3/2.
+ */
+public class WebConfig implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        
+    }
+}
